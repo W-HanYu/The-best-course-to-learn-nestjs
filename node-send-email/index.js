e088f741ffd896a4fe33c1585ecee2fda1210748
wangtao <1453300745@qq.com>
2023-12-03 13:19:34 +0800

    feat: use node send eamil

---
+++ node-send-email/index.js
@@ -0,0 +1,25 @@
+const nodemailer = require("nodemailer");
+const fs = require("fs");
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.qq.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: "nannan@qq.com",
+    pass: "lderrkjyvkegjijb", //你的授权码,
+  },
+});
+
+async function main() {
+  const info = await transporter.sendMail({
+    from: '"楠哥" <nannan@qq.com>',
+    to: "taotao@qq.com", //"nannan@163.com",
+    subject: "Hello 111",
+    html: fs.readFileSync("./index.html"),
+  });
+
+  console.log("邮件发送成功：", info.messageId);
+}
+
+main().catch(console.error);
---
+++ node-send-email/index2.js
@@ -0,0 +1,65 @@
+const { MailParser } = require("mailparser");
+const fs = require("fs");
+const path = require("path");
+const Imap = require("imap");
+
+function handleResults(results) {
+  imap
+    .fetch(results, {
+      bodies: "",
+    })
+    .on("message", (msg) => {
+      const mailparser = new MailParser();
+
+      msg.on("body", (stream) => {
+        const info = {};
+        stream.pipe(mailparser);
+
+        mailparser.on("headers", (headers) => {
+          info.theme = headers.get("subject");
+          info.form = headers.get("from").value[0].address;
+          info.mailName = headers.get("from").value[0].name;
+          info.to = headers.get("to").value[0].address;
+          info.datatime = headers.get("date").toLocaleString();
+        });
+
+        mailparser.on("data", (data) => {
+          if (data.type === "text") {
+            info.html = data.html;
+            info.text = data.text;
+            console.log(info);
+          }
+          if (data.type === "attachment") {
+            const filePath = path.join(__dirname, "files", data.filename);
+            const ws = fs.createWriteStream(filePath);
+            data.content.pipe(ws);
+          }
+        });
+      });
+    });
+}
+
+const imap = new Imap({
+  user: "nannan@qq.com",
+  password: "lderrkjyvkegjijb",
+  host: "imap.qq.com",
+  port: 993,
+  tls: true,
+});
+
+imap.once("ready", () => {
+  imap.openBox("INBOX", true, (err) => {
+    imap.search(
+      [["SEEN"], ["SINCE", new Date("2023-07-10 19:00:00").toLocaleString()]],
+      (err, results) => {
+        if (!err) {
+          handleResults(results);
+        } else {
+          throw err;
+        }
+      }
+    );
+  });
+});
+
+imap.connect();
